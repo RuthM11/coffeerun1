697e273c894b6051e893880c83cc060a5388a106
RuthM11 <ruth.madrid11@gmail.com>
2017-09-25 19:10:24 -0700

    Delete test_truck.js

--- CoffeeRun/tests/test_truck.js
+++
@@ -1,7 +0,0 @@
-myTruck.createOrder({email:'me@goldfinger.com', coffee: 'double mocha'});
-myTruck.createOrder({email:'dr@who.com', coffee: 'decaf'});
-myTruck.createOrder({email:'m@bond.com', coffee: 'ear grey'});
-myTruck.printOrders();
-myTruck.deliverOrder('dr@who.com');
-myTruck.deliverOrder('m@bond.com');
-myTruck.printOrders();
